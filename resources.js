db49ab2517a7714b3677975a39f099ac6fd0f3fc
userse31 <61165364+userse31@users.noreply.github.com>
2024-11-10 11:06:27 -0500

    Trans solidarity forever!

---
+++ resources.js
@@ -0,0 +1,7 @@
+// I'd include the snippit from the actual rom itself, I really would! But unfortunately capitalist IP law is a CUNT! Sorry.
+//Snippit range from the original rom is from $6000 to $686F, comprising of 2160 bytes.
+
+//The hard drive with my Debian install decided to start EOL'ing itself so I'm "stuck" using Firefox 52 under Windows Vista.
+//Firefox 52 _might_ support "const" and "uint8array" but I'm not confident so....
+//var font_thing=[0x1f,0x1f,0x1f ... 0x0c,0x00,0x00];
+var font_thing=[];
